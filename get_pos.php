6b86e1111acfa119ed45b8b482b3bbb83969aeda
Geoff St. Pierre <serundeputy@gmail.com>
2017-04-01 08:26:56 -0400

    Issue #2: Get a more accurate count of files downloaded.

--- get_pos.php
+++ get_pos.php
@@ -1,13 +1,28 @@
 <?php
 
+/**
+ * @file
+ * Download translation files from ftp.drupal.org.
+ *
+ * Be sure to modify the $drupal_path variable to point at the source code
+ * of a Drupal site on your computer.
+ *
+ * Usage: php get_pos.php
+ */
+
+// Drupal path.
+$drupal_path = "/Users/geoff/sites/flat/drupal-7.53";
+
 // Get drupal translation files.
-require_once "/Users/geoff/sites/flat/drupal-7.53/includes/iso.inc";
+require_once "$drupal_path/includes/iso.inc";
 
 $language_list = _locale_get_predefined_list();
 
+passthru("mkdir -p lang-files");
+
 foreach ($language_list as $key => $l) {
   print "\t$key\n";
-  passthru("wget https://ftp.drupal.org/files/translations/7.x/drupal/drupal-7.54.$key.po");
+  passthru("wget --directory-prefix=lang-files https://ftp.drupal.org/files/translations/7.x/drupal/drupal-7.54.$key.po");
 }
-$count = exec("ls -1 | wc -l");
-print "\n\ndone: $count files downloaded.\n";
+$count = exec("ls lang-files/*.po -1 | wc -l");
+print "\n\n\tdone: $count files downloaded.\n\n";
